d451e256c14a76bd8ebc1ec04624142730444e3b
Andy wu <543473183@qq.com>
2020-04-21 22:06:30 +0800

    分类

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,7 @@
     <div class="wrap">
         <router-view></router-view>
         <van-tabbar route v-model="active">
-            <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
+            <van-tabbar-item to="/" icon="home-o">首页2</van-tabbar-item>
             <van-tabbar-item to="/category" icon="apps-o">分类</van-tabbar-item>
             <van-tabbar-item to="/h" icon="shopping-cart-o">购物车</van-tabbar-item>
             <van-tabbar-item to="/h" icon="friends-o">我</van-tabbar-item>
